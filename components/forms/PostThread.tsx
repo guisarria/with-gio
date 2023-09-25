a773058b37e71ac10f1efb1f9b12ec0723fc6d2a
Guilherme <guisarria@gmail.com>
2023-09-25 20:25:36 -0300

    feat: Post Thread

---
+++ components/forms/PostThread.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { useForm } from 'react-hook-form'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { Textarea } from '../ui/textarea'
+import { Button } from '../ui/button'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { UserValidation } from '@/lib/validations/user'
+import * as z from 'zod'
+import Image from 'next/image'
+import { ChangeEvent, useState } from 'react'
+import { isBase64Image } from '@/lib/utils'
+import { useUploadThing } from '@/lib/uploadthing'
+import { updateUser } from '@/lib/actions/user.actions'
+import { usePathname, useRouter } from 'next/navigation'
+
+interface Props {
+  user: {
+    id: string
+    objectId: string
+    username: string
+    name: string
+    bio: string
+    image: string
+  }
+  btnTitle: string
+}
+
+function PostThread({ userId }: { userId: string }) {
+  const [files, setFiles] = useState<File[]>([])
+  const { startUpload } = useUploadThing('media')
+  const router = useRouter()
+  const pathname = usePathname()
+
+  const form = useForm<z.infer<typeof UserValidation>>({
+    resolver: zodResolver(UserValidation),
+    defaultValues: {
+      profile_photo: user?.image || '',
+      name: user?.name || '',
+      username: user?.username || '',
+      bio: user?.bio || '',
+    },
+  })
+
+  return <h1>Post Thread Form</h1>
+}
+
+export default PostThread
