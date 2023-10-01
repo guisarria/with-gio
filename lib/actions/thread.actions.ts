e3484f3dda18a9bcf0c937ec3705c9de055910c3
Guilherme <guisarria@gmail.com>
2023-10-01 02:45:08 -0300

    feat: profile page

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -75,10 +75,73 @@ export async function fetchThreadById(id: string) {
 
   try {
     // TODO: Populate Community
-    const thread = await Thread.findById(id).populate({
-      path: 'author',
-      model: User,
-      select: '_id id name image',
+    const thread = await Thread.findById(id)
+      .populate({
+        path: 'author',
+        model: User,
+        select: '_id id name image',
+      })
+      .populate({
+        path: 'children',
+        populate: [
+          {
+            path: 'author',
+            model: User,
+            select: '_id id name parentId image',
+          },
+          {
+            path: 'children',
+            model: Thread,
+            populate: {
+              path: 'author',
+              model: User,
+              select: '_id id name parentId image',
+            },
+          },
+        ],
+      })
+      .exec()
+
+    return thread
+  } catch (error: any) {
+    throw new Error(`Error fetching thread: ${error.message}`)
+  }
+}
+
+export async function addCommentToThread(
+  threadId: string,
+  commentText: string,
+  userId: string,
+  path: string
+) {
+  connectToDB()
+
+  try {
+    // Find the original thread by its ID
+    const originalThread = await Thread.findById(threadId)
+
+    if (!originalThread) {
+      throw new Error('Thread not found')
+    }
+
+    // Create a new thread with the comment text
+    const commentThread = new Thread({
+      text: commentText,
+      author: userId,
+      parentId: threadId,
     })
-  } catch (error) {}
+
+    // Save the new thread
+    const savedCommentThread = await commentThread.save()
+
+    // Update the original thread to include the new comment
+    originalThread.children.push(savedCommentThread._id)
+
+    // Save the original thread
+    await originalThread.save()
+
+    revalidatePath(path)
+  } catch (error: any) {
+    throw new Error(`Error adding comment to thread: ${error.message}`)
+  }
 }
