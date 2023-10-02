ca40e12b4e644f7451cacb92b7b891d1fd10b45c
Guilherme <guisarria@gmail.com>
2023-10-02 01:27:25 -0300

    finish

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -12,11 +12,9 @@ import CommunityCard from '@/components/cards/CommunityCard'
 
 async function Page() {
   const user = await currentUser()
-
   if (!user) return null
 
   const userInfo = await fetchUser(user.id)
-
   if (!userInfo?.onboarded) redirect('/onboarding')
 
   const result = await fetchCommunities({
